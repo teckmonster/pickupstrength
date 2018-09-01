8b97da884f74338f193a4fe6b35bd2184a880958
Petite Weightlifter <petiteweightlifter@gmail.com>
2018-08-31 20:49:08 -0400

    update font-family in pinterest.js

--- js/pinterest.js
+++ js/pinterest.js
@@ -6,3 +6,9 @@
     p.src = '//assets.pinterest.com/js/pinit.js';
     f.parentNode.insertBefore(p, f);
 }(document));
+
+$(function() {
+    var fonts = 'https://fonts.googleapis.com/css?family=Volkhov:400,700|Open+Sans:400,700|Spectral:400,700';
+    if (document.createStyleSheet) document.createStyleSheet(fonts);
+    else $("head").append($("<link rel='stylesheet' href='"+ fonts +"' type='text/css' media='screen' />"));
+});
